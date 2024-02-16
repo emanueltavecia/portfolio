3e4c16dce17500478888adb291c23d9117b4a16b
Emanuel Tavecia <emanueltavecia@hotmail.com>
2024-02-16 19:28:57 +0000

    initial commit

---
+++ src/app/components/about/index.tsx
@@ -0,0 +1,11 @@
+export function About() {
+  return(
+    <div id="about" className="flex justify-between items-center">
+      <div>
+        <h1 className="text-4xl font-bold mb-2">Olá, sou o Emanuel Tavecia</h1>
+        <p className="text-lg">Estudante de programação Front-End</p>
+      </div>
+      <img className="w-40 rounded-full" src="https://github.com/emanueltavecia.png" alt="Foto de perfil de Emanuel Tavecia" />
+    </div>
+  )
+}
---
+++ src/app/components/header/index.tsx
@@ -0,0 +1,14 @@
+export function Header() {
+  return(
+    <header className="flex justify-between py-9">
+      <span className="font-bold">Emanuel Tavecia</span>
+      <nav>
+        <ul className="flex gap-7 font-medium">
+          <li><a href="#projects">Projetos</a></li>
+          <li><a href="#about">Certificações</a></li>
+          <li><a href="#contact">Contato</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
