f0b3ea58763556466545925ca911570e374b80c0
Emanuel Tavecia <emanueltavecia@hotmail.com>
2024-02-16 21:28:53 +0000

    add responsive layout

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,9 +1,9 @@
 export function Header() {
   return(
-    <header className="flex justify-between py-9">
-      <span className="font-bold">Emanuel Tavecia</span>
+    <header className="flex flex-col md:flex-row gap-4 items-center justify-between py-9">
+      <span className="font-bold text-xl md:font-lg">Emanuel Tavecia</span>
       <nav>
-        <ul className="flex gap-7 font-medium">
+        <ul className="flex gap-5 md:gap-7 font-medium">
           <li><a href="#projects">Projetos</a></li>
           <li><a href="#certificates">Certificações</a></li>
           <li><a href="#contact">Contato</a></li>
