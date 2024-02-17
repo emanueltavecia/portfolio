5b47192b0913f9d6524cf2fb381d5fda5b3bdddf
Emanuel Tavecia <emanueltavecia@hotmail.com>
2024-02-17 16:41:29 +0000

    change page language to pt-br and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Emanuel Tavecia",
-  description: "Emanuel Tavecia Portfolio",
+  description: "Portfólio de Emanuel Tavecia",
 };
 
 export default function RootLayout({
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body className={inter.className}>{children}</body>
     </html>
   );
