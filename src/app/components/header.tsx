f7b918619769372eed5bb08c8b5ce91d251918d6
Emanuel Tavecia <emanueltavecia@hotmail.com>
2024-02-17 02:43:14 +0000

    add background color

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,14 +1,16 @@
 export function Header() {
   return(
-    <header className="flex flex-col md:flex-row gap-4 items-center justify-between py-9">
-      <span className="font-bold text-xl md:font-lg">Emanuel Tavecia</span>
-      <nav>
-        <ul className="flex gap-5 md:gap-7 font-medium">
-          <li><a href="#projects">Projetos</a></li>
-          <li><a href="#certificates">Certificações</a></li>
-          <li><a href="#contact">Contato</a></li>
-        </ul>
-      </nav>
-    </header>
+    <div className="bg-zinc-950 ">
+      <header className="max-w-5xl w-full mx-auto px-4 md:px-8 flex flex-col md:flex-row gap-4 items-center justify-between py-9">
+        <span className="font-bold text-xl md:font-lg">Emanuel Tavecia</span>
+        <nav>
+          <ul className="flex gap-5 md:gap-7 font-medium">
+            <li><a href="#projects">Projetos</a></li>
+            <li><a href="#certificates">Certificações</a></li>
+            <li><a href="#contact">Contato</a></li>
+          </ul>
+        </nav>
+      </header>
+    </div>
   )
 }
