983aec8a367c1bcea82712886cdf36cd32947313
Emanuel Tavecia <emanueltavecia@hotmail.com>
2024-02-17 16:42:09 +0000

    change bg color

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,8 +1,8 @@
 export function Footer() {
   return (
-    <footer className="bg-zinc-950 text-center py-10 mt-16 flex flex-col gap-3">
+    <footer className="bg-zinc-900 text-center py-10 mt-16 flex flex-col gap-3 rounded-t-lg">
       <a className="underline font-bold" href="#top">Voltar ao início</a>
-      <p>Criado com Next.js por Emanuel Tavecia</p>
+      <p>Desenvolvido com Next.js por Emanuel Tavecia</p>
     </footer>
   )
 }
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,7 +1,7 @@
 export function Header() {
   return(
-    <div className="bg-zinc-950 ">
-      <header className="max-w-5xl w-full mx-auto px-4 md:px-8 flex flex-col md:flex-row gap-4 items-center justify-between py-9">
+    <div className="bg-zinc-900 rounded-b-lg">
+      <header className="max-w-5xl w-full mx-auto px-4 md:px-8 flex flex-col md:flex-row gap-4 items-center justify-between py-9 ">
         <span className="font-bold text-xl md:font-lg">Emanuel Tavecia</span>
         <nav>
           <ul className="flex gap-5 md:gap-7 font-medium">
