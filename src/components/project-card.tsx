f6f531aa272c2adafec239c77a3bafc0a9835077
Emanuel Tavecia <emanueltavecia@hotmail.com>
2024-02-21 22:55:50 -0300

    add preview link on project cover

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -1,4 +1,4 @@
-import { Eye, Github } from "lucide-react";
+import { Eye, Github } from 'lucide-react'
 
 interface ProjectCardProps {
   project: {
@@ -12,13 +12,29 @@ export function ProjectCard({ project }: ProjectCardProps) {
   return (
     <div className="flex flex-col justify-between gap-4 bg-slate-900 rounded-md p-5 transition-all duration-200 hover:scale-105">
       <p className="text-lg font-semibold text-center">{project.name}</p>
-      <img className="rounded-md" src={`https://github.com/emanueltavecia/${project.repo_name}/blob/main/.github/screenshot.png?raw=true`} alt={`Capa do projeto ${project.name}`} />
+
+      <a target='_blank' href={project.deploy}>
+        <img
+          className="rounded-md"
+          src={`https://github.com/emanueltavecia/${project.repo_name}/blob/main/.github/screenshot.png?raw=true`}
+          alt={`Capa do projeto ${project.name}`}
+        />
+      </a>
+
       <div className="flex justify-between">
-        <a href={project.deploy} target="_blank" className="flex items-center gap-2">
+        <a
+          href={project.deploy}
+          target="_blank"
+          className="flex items-center gap-2"
+        >
           <Eye />
           <p>Preview</p>
         </a>
-        <a href={`https://github.com/emanueltavecia/${project.repo_name}`} target="_blank" className="flex items-center gap-2">
+        <a
+          href={`https://github.com/emanueltavecia/${project.repo_name}`}
+          target="_blank"
+          className="flex items-center gap-2"
+        >
           <Github />
           <p>Repositório</p>
         </a>
