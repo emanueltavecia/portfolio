b95273a0bb3bc94812c8dae449c43270d9304ec5
Emanuel Tavecia <emanueltavecia@hotmail.com>
2024-02-25 20:12:34 +0000

    update header background blur

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 export function Header() {
   return(
-    <div className="bg-zinc-900/80 backdrop-blur-lg rounded-b-lg fixed top-0 left-0 right-0">
+    <div className="bg-zinc-900/70 backdrop-blur-md rounded-b-lg fixed top-0 left-0 right-0">
       <header className="max-w-5xl w-full mx-auto px-4 py-5 md:py-7 md:px-8 flex flex-col md:flex-row gap-4 items-center justify-between">
         <span className="font-bold text-xl md:font-lg">Emanuel Tavecia</span>
         <nav>
