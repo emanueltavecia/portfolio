72c4e0a96277abd8aa2a7f797e6ef0651a80fa3d
Emanuel Tavecia <emanueltavecia@hotmail.com>
2024-02-25 20:36:15 +0000

    change back to top link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,12 @@
+import { ArrowUp } from "lucide-react";
+
 export function Footer() {
   return (
-    <footer className="bg-zinc-900 text-center py-10 mt-16 flex flex-col gap-3 rounded-t-lg">
-      <a className="underline font-bold" href="#top">Voltar ao início</a>
-      <p>Desenvolvido com Next.js por Emanuel Tavecia</p>
+    <footer className="bg-zinc-900 items-center py-8 px-5 mt-16 flex flex-col gap-4 rounded-t-lg">
+      <p className="text-center font-medium">Desenvolvido com Next.js por Emanuel Tavecia</p>
+      <a href="#top" className="p-2 bg-slate-900 rounded-md border-2 border-zinc-500">
+        <ArrowUp />
+      </a>
     </footer>
   )
 }
